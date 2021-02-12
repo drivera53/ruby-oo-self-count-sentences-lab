5935bcba5bf01690f2261594a96bcc84b253031d
Danny Rivera <samboard@hotmail.com>
2021-02-12 09:39:49 -0500

    commit tree 927c0f0374e91fae9321ac304838f3095ad65f70

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,42 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
 
   end
 
   def exclamation?
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
 
   end
 
   def count_sentences
-
+    first_pass = self.split("! ")
+    second_pass = first_pass.collect do |sentence|
+      sentence.split("? ")
+    end
+    temp_third = second_pass.flatten
+    third_pass = temp_third.collect do |sentence|
+      sentence.split(". ")
+    end
+    @total = third_pass.flatten.count
   end
 end
+
+#binding.pry
